d7e27cfaf4aa29d74b2d243e82e034ebf683e9c6
Edward Shin <shinedwardc@gmail.com>
2022-01-28 01:25:57 -0500

    Made some changes/fixes

--- lab2/weather.js
+++ lab2/weather.js
@@ -61,6 +61,36 @@ $(document).ready(function() {
             document.getElementById("status").innerHTML = data.weather[0].description;
             createDescription(data);
             document.getElementById("image").innerHTML = AddIcon(data);
+            switch (data.weather[0].main) {
+                case "Snow":
+                  document.getElementById("wrapper-bg").style.backgroundImage =
+                    "url('https://mdbgo.io/ascensus/mdb-advanced/img/snow.gif')";
+                  break;
+                case "Clouds":
+                  document.getElementById("wrapper-bg").style.backgroundImage =
+                    "url('https://mdbgo.io/ascensus/mdb-advanced/img/clouds.gif')";
+                  break;
+                case "Fog":
+                  document.getElementById("wrapper-bg").style.backgroundImage =
+                    "url('https://mdbgo.io/ascensus/mdb-advanced/img/fog.gif')";
+                  break;
+                case "Rain":
+                  document.getElementById("wrapper-bg").style.backgroundImage =
+                    "url('https://mdbgo.io/ascensus/mdb-advanced/img/rain.gif')";
+                  break;
+                case "Clear":
+                  document.getElementById("wrapper-bg").style.backgroundImage =
+                    "url('https://mdbgo.io/ascensus/mdb-advanced/img/clear.gif')";
+                  break;
+                case "Thunderstorm":
+                  document.getElementById("wrapper-bg").style.backgroundImage =
+                    "url('https://mdbgo.io/ascensus/mdb-advanced/img/thunderstorm.gif')";
+                  break;
+                default:
+                  document.getElementById("wrapper-bg").style.backgroundImage =
+                    "url('https://mdbgo.io/ascensus/mdb-advanced/img/clear.gif')";
+                  break;
+              }
         });
     });
 });
