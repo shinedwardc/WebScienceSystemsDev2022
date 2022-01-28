6d508d7cdab83c67f1082da15355f8a319c484f6
Edward Shin <shinedwardc@gmail.com>
2022-01-28 00:46:32 -0500

    Move files
    Upload files to github

---
+++ lab2/weather.js
@@ -0,0 +1,66 @@
+function Decimal_2(num){
+    return (Math.round(num * 100)/100).toFixed(2);
+}
+
+function temp(value){
+    var f = (value - 273.15) * 1.8 + 32;
+    return Decimal_2(f) + ' \xB0F';
+}
+
+function MetersToMiles(value){
+    var m = (value * 2.236936)
+    return Decimal_2(m) + ' mph';
+}
+
+function createDescription(data){
+    var description = document.getElementById('description');
+    var str = "";
+    str += '<i class = "fas fa-wind fa-fw" style="color: #cabec0;"></i>';
+    str += '<span class = "ms-1">';
+    str += MetersToMiles(data.wind.speed);
+    str += '</span></div>';
+    str += '<div><i class = "fas fa-tint fa-fw" style="color: #d4f1f9;"></i>';
+    str += '<span class = "ms-1">';
+    str += data.main.humidity + '%';
+    description.innerHTML = str;
+}
+
+function AddIcon(data){
+    var link = "http://openweathermap.org/img/w/" + data.weather[0].icon + '.png';
+    return "<img src = " + link + ">";
+}
+
+$(document).ready(function() {
+    navigator.geolocation.getCurrentPosition(function(getPosition) {
+        let lat = getPosition.coords.latitude;
+        let long = getPosition.coords.longitude;
+        let key = "9c0723a59cec2b9edeaa774538f8ed7d";
+        console.log(lat);
+        console.log(long);
+        let link = 'http://api.openweathermap.org/data/2.5/weather?lat=';
+        //let link2 = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&units=metric';
+        link += lat;
+        link += '&lon=' + long;
+        link += '&appid=' + key;
+        console.log(link);
+        //console.log(link2);
+        $.getJSON(link, function(data) {
+            //console.log(data.name);
+            document.getElementById("city").innerHTML = data.name;
+            var today = new Date();
+            //var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+            //var dateTime = date+' '+time;
+            var dd = String(today.getDate()).padStart(2, '0');
+            var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+            var yyyy = today.getFullYear();
+            var date = mm + '/' + dd + '/' + yyyy;
+            document.getElementById("date").innerHTML = date;
+            //console.log(data.main.temp);
+            document.getElementById("temp").innerHTML = temp(data.main.temp);
+            //console.log(data.weather[0].description);
+            document.getElementById("status").innerHTML = data.weather[0].description;
+            createDescription(data);
+            document.getElementById("image").innerHTML = AddIcon(data);
+        });
+    });
+});
