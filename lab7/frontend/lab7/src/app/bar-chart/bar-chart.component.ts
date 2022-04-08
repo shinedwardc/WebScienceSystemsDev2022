d5524b295104915e62a852a1764c145c784dad4a
Edward Shin <shinedwardc@gmail.com>
2022-04-08 01:46:04 -0400

    Update bar-chart.component.ts

--- lab7/frontend/lab7/src/app/bar-chart/bar-chart.component.ts
+++ lab7/frontend/lab7/src/app/bar-chart/bar-chart.component.ts
@@ -10,13 +10,6 @@ import * as d3 from 'd3';
 })
 export class BarChartComponent implements OnInit {
   private data : any = [];
-  private haha = [
-    {"Framework": "Vue", "Stars": "166443", "Released": "2014"},
-    {"Framework": "React", "Stars": "150793", "Released": "2013"},
-    {"Framework": "Angular", "Stars": "62342", "Released": "2016"},
-    {"Framework": "Backbone", "Stars": "27647", "Released": "2010"},
-    {"Framework": "Ember", "Stars": "21471", "Released": "2011"},
-  ];
   private svg: any;
   private margin = 50;
   private width = 750 - (this.margin * 2);
